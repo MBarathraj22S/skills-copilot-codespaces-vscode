d146a660c174c2f6a32706d4c9e8492de1ea427d
BARATHRAJ M <barathrajmcse2022@ksrce.ac.in>
2025-02-18 18:01:07 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -2,13 +2,28 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
 //create a route
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('Hello World!');
+});
+//listen for requests
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
+//create a comment
+app.post('/comments', (req, res) => {
+    res.send('Comment created');
+});
+//read a comment
+app.get('/comments/:id', (req, res) => {
+    res.send('Comment with id: ' + req.params.id);
+});
+//update a comment
+app.put('/comments/:id', (req, res) => {
+    res.send('Comment updated');
+});
+//delete a comment
+app.delete('/comments/:id', (req, res) => {
+    res.send('Comment deleted');
 });
 
-//start the server
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
